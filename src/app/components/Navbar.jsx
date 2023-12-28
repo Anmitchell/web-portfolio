5eec41191cdd2f17f72fe67f57ec439960265c1f
Anmitchell <avarynmitchell@gmail.com>
2023-12-27 22:35:07 -0500

    added navbar component

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Link from 'next/link'
+//import Button from './Button'
+
+const Navbar = () => {
+  return <h1 className='text-white'>Navbar Here</h1>
+}
+
+export default Navbar
